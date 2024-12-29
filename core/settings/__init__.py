e8c959a32ac6bc0a46c8efdb5427710a90ba6b0a
officialalkenes <belloabdulhakeemolamide@gmail.com>
2024-12-29 08:53:53 +0100

    fix: base configuration installation mismatch fixed

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,6 +1,15 @@
-from decouple import config
+import os
+from pathlib import Path
 
-DJANGO_ENVIRONMENT = config("ENVIRONMENT", "dev")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+DJANGO_ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
 
 if DJANGO_ENVIRONMENT == "dev":
     from .dev import *  # noqa: F403
