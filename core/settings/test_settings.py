33daf57e61d12a43b5fd084b6a5420b3297da38c
officialalkenes <belloabdulhakeemolamide@gmail.com>
2025-01-01 10:29:50 +0100

    fix 1: modified Database connection

--- core/settings/test_settings.py
+++ core/settings/test_settings.py
@@ -2,9 +2,13 @@ import os
 from urllib.parse import urlparse
 from .base import *  # noqa
 
+database_url = os.getenv("DATABASE_URL")
+if isinstance(database_url, bytes):
+    # Decode the bytes-like object to a string
+    database_url = database_url.decode("utf-8")
 
-tmpPostgres = urlparse(os.getenv("DATABASE_URL"))  # noqa
-print(tmpPostgres)
+tmpPostgres = urlparse(database_url)
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
